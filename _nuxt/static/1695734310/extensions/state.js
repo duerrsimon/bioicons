8276bcbe586a26c5141f9f978e6b41b45f18bcc0
duerrsimon <duerrsimon@users.noreply.github.com>
2023-09-26 13:19:11 +0000

    deploy: d63573c0a939c54f5f67d92c0cf02f29e0dccc0c

---
+++ _nuxt/static/1695734310/extensions/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1695734310",layout:"default",error:a,serverRendered:true,routePath:"\u002Fextensions",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
