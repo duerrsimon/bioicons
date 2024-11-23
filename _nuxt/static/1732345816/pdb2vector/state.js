d30db4059dbfe48855074c00e47faeb4f6815db0
duerrsimon <duerrsimon@users.noreply.github.com>
2024-11-23 07:10:41 +0000

    deploy: a29154a9207836f53aed3fe1fff53864393819ce

---
+++ _nuxt/static/1732345816/pdb2vector/state.js
@@ -0,0 +1,1 @@
+window.__NUXT__=(function(a){return {staticAssetsBase:"\u002F_nuxt\u002Fstatic\u002F1732345816",layout:"default",error:a,serverRendered:true,routePath:"\u002Fpdb2vector",config:{app:{basePath:"\u002F",assetsPath:"\u002F_nuxt\u002F",cdnURL:a}}}}(null));
